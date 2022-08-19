da74d2b9e06c14a1aea5411b1a7e9206488b5945
Luca-Furegon-886933 <79788062+Luca-Furegon-886933@users.noreply.github.com>
2022-08-19 11:24:27 +0200

    Add files via upload

---
+++ blueprints/login.py
@@ -0,0 +1,68 @@
+import secrets
+from flask import Flask, redirect, url_for, render_template, make_response, request
+from flask_login import *
+from sqlalchemy.sql import select
+from sqlalchemy import insert
+from database import *
+
+app=Flask(__name__)
+secret_key = secrets.token_hex(16)
+app.config['SECRET_KEY'] = secret_key
+
+login_manager=LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader # attenzione a questo !
+def get_user_by_email(email):
+    conn = engine.connect()
+    res = conn.execute(select([Users]).where(Users.c.email == email))
+    user = res.fetchone()
+    conn.close()
+    return Users(user.email, user.Name, user.BirthDate, user.Country, user.Gender, user.Password)
+
+@app.route('/')
+def home():
+    # current_user identifica l’utente attuale
+    # utente anonimo prima dell ’ autenticazione
+    if current_user.is_authenticated :
+        return redirect(url_for('private'))
+    return render_template("home.html")
+
+@app.route('/private')
+@login_required # richiede autenticazione
+def private():
+    conn = engine.connect()
+    all_users = conn.execute(select([Users]))
+    resp = make_response(render_template("private.html", users = all_users))
+    conn.close()
+    return resp
+
+
+@app.route('/login',methods=['GET', 'POST'])
+def login ():
+    if request.method == 'POST':
+        conn = engine.connect()
+        res = conn.execute(select([Users.Password]).where(Users.email == request.form["user"]))
+        real_pwd = res.fetchone ()
+        conn.close ()
+        if(real_pwd is not None and request.form["pass"] == real_pwd["pwd"]):
+            user = get_user_by_email(request.form["user"])
+            login_user(user) # chiamata a Flask - Login
+            return redirect(url_for('private'))    
+        else:
+            return redirect(url_for('home'))
+    else:
+        return redirect(url_for('home'))
+
+
+@app.route('/logout')
+@login_required # richiede autenticazione
+def logout():
+    logout_user () # chiamata a Flask - Login
+    return redirect(url_for('home'))
+
+@app.route('/subscribe')
+def subscribe():
+    return render_template("subscribe.html")
+
