0b1cdce739c807a5a247fb82bc4dd07b42a012a8
Luca_Furegon_886933 <886933@stud.gmail.com>
2022-08-21 13:04:49 +0200

    profile update

--- blueprints/profile/routes.py
+++ blueprints/profile/routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask import current_app as app
 from flask_login import *
+from blueprints.models import *
+
 
 login_manager=LoginManager()
 login_manager.init_app(app)
@@ -16,3 +18,16 @@ profile_bp = Blueprint(
 @login_required # richiede autenticazione
 def profile():
     return render_template("profile.html", user = current_user)
+
+@profile_bp.route('/update')
+@login_required
+def update():
+    return render_template("update_info.html", user = current_user)
+
+@profile_bp.route('/update_info/<string:Email>', methods=['GET', 'POST']) 
+@login_required
+def update_info(Email): 
+    if request.method == 'POST':
+        session.query(Users).filter(Users.Email == Email).update({Users.Name : request.form["name"]})
+        session.commit()
+        return redirect(url_for('profile_bp.profile'))
