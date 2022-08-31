ad6aa77d73c26324a2ded1d02d6b718fbb03dc03
Martino-Pistellato <smart.piste@gmail.com>
2022-08-31 21:15:35 +0200

    stats.html

--- blueprints/stats/routes.py
+++ blueprints/stats/routes.py
@@ -24,31 +24,9 @@ def stats():
         playlists = session.query(Playlists).filter(Playlists.User == current_user.Email)
             
         my_songs=session.query(Songs).filter(Songs.Artist==current_user.Email).order_by(Songs.N_Likes.desc())
-        my_albums=session.query(Albums).filter(Albums.Artist==current_user.Email).order_by(Albums.N_Likes.desc())
-            
-        all_liked_songs=session.query(Users_liked_Songs.song_id)
-        my_liked_songs=session.query(Songs.Id).filter(and_(Songs.Artist==current_user.Email,Songs.Id.in_(all_liked_songs)))
-        users_like_songs=session.query(Users.Email).filter(Users.Email.in_(session.query(Users_liked_Songs.user_email).filter(Users_liked_Songs.song_id.in_(my_liked_songs))))
-            
-        all_liked_albums=session.query(Users_liked_Albums.album_id)
-        my_liked_albums=session.query(Albums.Id).filter(and_(Albums.Artist==current_user.Email,Albums.Id.in_(all_liked_albums)))
-        users_like_albums=session.query(Users.Email).filter(Users.Email.in_(session.query(Users_liked_Albums.user_email).filter(Users_liked_Albums.album_id.in_(my_liked_albums))))
-            
-        users_like_me=session.query(Users).filter(or_(Users.Email.in_(users_like_songs), Users.Email.in_(users_like_albums))).all()
-            
-        medium_age = 0
-        for x in users_like_me:
-            medium_age += 2022-x.BirthDate.year
+        my_albums=session.query(Albums).filter(Albums.Artist==current_user.Email).order_by(Albums.N_Likes.desc())    
         
-        if(len(users_like_me)>0):
-            medium_age /= len(users_like_me)    
-        
-        countries=session.query(Users.Country, func.count(Users.Email)).filter(or_(Users.Email.in_(users_like_songs), Users.Email.in_(users_like_albums))).group_by(Users.Country).all()
-            
-        #countries = session.query(users_like_me.Country,func.count(users_like_me.Email)).group_by(users_like_me.Country)
-        #countries = session.query(Users.Country).filter(Users.Country.in_(users_like_me.Countr))    
-        
-        return render_template("stats.html", user=current_user, playlists=playlists, users=users_like_me, my_songs=my_songs, my_albums=my_albums, countries=countries)
+        return render_template("stats.html", user=current_user, playlists=playlists, my_songs=my_songs, my_albums=my_albums)
     return redirect(url_for('home_bp.home'))
 
 @stats_bp.route('/get_countries')
@@ -60,14 +38,62 @@ def get_countries():
     all_liked_albums=session.query(Users_liked_Albums.album_id)
     my_liked_albums=session.query(Albums.Id).filter(and_(Albums.Artist==current_user.Email,Albums.Id.in_(all_liked_albums)))
     users_like_albums=session.query(Users.Email).filter(Users.Email.in_(session.query(Users_liked_Albums.user_email).filter(Users_liked_Albums.album_id.in_(my_liked_albums))))
-        
+    
     countries=session.query(Users.Country, func.count(Users.Email)).filter(or_(Users.Email.in_(users_like_songs), Users.Email.in_(users_like_albums))).group_by(Users.Country).all()
     
-    #l=[]
     res={}
     for c in countries:
-        #dict={}
         res[c[0]] = c[1]
-    #l.append(dict)
+    
+    return jsonify({'dati':res})
+
+@stats_bp.route('/get_ages')
+def get_ages():
+    
+    all_liked_songs=session.query(Users_liked_Songs.song_id)
+    my_liked_songs=session.query(Songs.Id).filter(and_(Songs.Artist==current_user.Email,Songs.Id.in_(all_liked_songs)))
+    users_like_songs=session.query(Users.Email).filter(Users.Email.in_(session.query(Users_liked_Songs.user_email).filter(Users_liked_Songs.song_id.in_(my_liked_songs))))
+        
+    all_liked_albums=session.query(Users_liked_Albums.album_id)
+    my_liked_albums=session.query(Albums.Id).filter(and_(Albums.Artist==current_user.Email,Albums.Id.in_(all_liked_albums)))
+    users_like_albums=session.query(Users.Email).filter(Users.Email.in_(session.query(Users_liked_Albums.user_email).filter(Users_liked_Albums.album_id.in_(my_liked_albums))))
+    
+    users_age=session.query(Users.BirthDate.year).filter(or_(Users.Email.in_(users_like_songs), Users.Email.in_(users_like_albums))).all()
+    
+    medium_age = 0
+    for x in users_age:
+        medium_age += 2022-x
+    
+    if(len(users_age)>0):
+        medium_age /= len(users_age)
+        
+    
+    
+    res={}
+    for age in users_age:
+        if 2022-x not in res:
+            res[2022-x] = 1
+        else:
+            res[2022-x] += 1
+    
+    
+    return jsonify({'dati':res})
+
+@stats_bp.route('/get_genders')
+def get_genders():
+    
+    all_liked_songs=session.query(Users_liked_Songs.song_id)
+    my_liked_songs=session.query(Songs.Id).filter(and_(Songs.Artist==current_user.Email,Songs.Id.in_(all_liked_songs)))
+    users_like_songs=session.query(Users.Email).filter(Users.Email.in_(session.query(Users_liked_Songs.user_email).filter(Users_liked_Songs.song_id.in_(my_liked_songs))))
+        
+    all_liked_albums=session.query(Users_liked_Albums.album_id)
+    my_liked_albums=session.query(Albums.Id).filter(and_(Albums.Artist==current_user.Email,Albums.Id.in_(all_liked_albums)))
+    users_like_albums=session.query(Users.Email).filter(Users.Email.in_(session.query(Users_liked_Albums.user_email).filter(Users_liked_Albums.album_id.in_(my_liked_albums))))
+    
+    users_gender=session.query(Users.Gender, func.count(Users.Email)).filter(or_(Users.Email.in_(users_like_songs), Users.Email.in_(users_like_albums))).group_by(Users.Gender).all()
+    
+    res={}
+    for gen in users_gender:
+        res[gen[0]] = gen[1]
     
     return jsonify({'dati':res})
